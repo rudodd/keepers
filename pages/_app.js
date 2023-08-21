0ee36f202578fad6568c191e30ee2c3791c2940b
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-21 14:09:46 -0500

    code cleanup and conversion to SASS

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
+import '../styles/globals.scss'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
