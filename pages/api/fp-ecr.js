4a6a44c56941f4f43e1414d063302b7852579306
Rustin Dodd <rustin.dodd@ibm.com>
2025-08-04 14:37:30 -0500

    updates the FP ECR scraping url and logic

--- pages/api/fp-ecr.js
+++ pages/api/fp-ecr.js
@@ -1,23 +1,18 @@
-// Import library functionality
-import * as cheerio from 'cheerio';
-
 // Scrape fantasy pros for current ECR values
 export default function handler(req, res) {
-  return fetch('https://www.fantasypros.com/nfl/cheatsheets/top-ppr-players.php')
+  return fetch('https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php')
     .then((response) => {
       if (response.ok) {
         return response.text()
           .then((text) => {
-            const $ = cheerio.load(text);
-            $.html();
-            const players = $('.player-list ul li').map(function(index) {
-              const obj = {
-                ecr: index + 1,
-                name: $(this).find('a:first-of-type').text(),
-                position: $(this).find('small').text().split('-')[0]
-              }
-              return obj;
-            }).get();
+            const variableName = 'ecrData';
+            const regex = new RegExp(`(?:var|let|const)\\s+${variableName}\\s*=\\s*(.*?);`);
+            const playersData = text.match(regex);
+            const players = JSON.parse(playersData[1]).players.map((player) => ({
+              ecr: player.rank_ecr,
+              name: player.player_name,
+              position: player.player_positions
+            }));
             return res.status(200).json(players);
           })
       } else {
