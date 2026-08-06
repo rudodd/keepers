86d9e6de6a3619ff4af6fc7b47de56b5aaf61652
Rustin Dodd <rustin.dodd@ibm.com>
2026-08-06 12:40:28 -0500

    updates prev keepers

--- keepers.js
+++ keepers.js
@@ -1,44 +1,12 @@
 const keepers = [
-  {
-    name: "Travis Etienne Jr.",
-    round: "5",
-    position: "RB"
-  },
-  {
-    name: "Breece Hall",
-    round: "5",
-    position: "RB"
-  },
-  {
-    name: "Kyren Williams",
-    round: "10",
-    position: "RB"
-  },
-  {
-    name: "Puka Nacua",
-    round: "10",
-    position: "WR"
-  },
-  {
-    name: "De'Von Achane",
-    round: "15",
-    position: "RB"
-  },
-  {
-    name: "Nico Collins",
-    round: "14",
-    position: "WR"
-  },
-  {
-    name: "Tyjae Spears",
-    round: "16",
-    position: "RB"
-  },
-  {
-    name: "Sam LaPorta",
-    round: "17",
-    position: "TE"
-  },
+  { name: 'Bucky Irving', round: '10', position: 'RB' },
+  { name: 'Jayden Daniels', round: '16', position: 'QB' },
+  { name: 'Joe Burrow', round: '8', position: 'QB' },
+  { name: 'Xavier Worthy', round: '9', position: 'WR' },
+  { name: 'Ladd McConkey', round: '11', position: 'WR' },
+  { name: 'Brock Bowers', round: '15', position: 'WR' },
+  { name: 'Brian Thomas Jr. ', round: '9', position: 'WR' },
+  { name: 'Malik Nabers', round: '5', position: 'WR' },
 ];
 
 export default keepers;
